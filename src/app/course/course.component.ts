ef8ae5fbfc0aa2aacb792c75f38adb69de0ff337
CristianAtienza27 <cristianatienza26@gmail.com>
2022-11-06 23:57:37 +0100

    Angular Material Data Table Data Selection

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -8,6 +8,7 @@ import {CoursesService} from "../services/courses.service";
 import { debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize } from 'rxjs/operators';
 import { merge, fromEvent, throwError } from 'rxjs';
 import { Lesson } from '../model/lesson';
+import { SelectionModel } from '@angular/cdk/collections';
 
 
 @Component({
@@ -29,12 +30,14 @@ export class CourseComponent implements OnInit, AfterViewInit {
     @ViewChild(MatSort)
     sort: MatSort;
 
+    selection = new SelectionModel<Lesson>(true, []);
+
     constructor(private route: ActivatedRoute,
                 private coursesService: CoursesService) {
 
     }
 
-    displayedColumns = ['seqNo', "description", "duration"];
+    displayedColumns = ['select','seqNo', "description", "duration"];
 
     expandedLesson: Lesson = null;
 
@@ -53,7 +56,15 @@ export class CourseComponent implements OnInit, AfterViewInit {
       else{
          this.expandedLesson = lesson;
       }
-      
+
+    }
+
+    onLessonToogled(lesson:Lesson) {
+
+      this.selection.toggle(lesson);
+
+      console.log(this.selection.selected);
+
     }
 
     loadLessonsPage() {
@@ -88,6 +99,19 @@ export class CourseComponent implements OnInit, AfterViewInit {
               tap(() => this.loadLessonsPage())
           )
           .subscribe();
+    }
+
+    isAllSelected() {
+      return this.selection.selected?.length == this.lessons?.length;
+    }
+
+    toggleAll() {
+      if (this.isAllSelected()) {
+        this.selection.clear();
+      }
+      else {
+        this.selection.select(...this.lessons);
+      }
     }
 
 }
